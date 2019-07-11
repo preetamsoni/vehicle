69ab16276e2d4878c5ad85acd95f9a71221c51bf
Rushit Vora <rushit.vora@gmail.com>
2019-07-11 18:41:12 +0530

    create Table and modal for images

---
+++ src/app/layout/constant.ts
@@ -0,0 +1,69 @@
+export const jsonData = {
+    'LdvResults': [
+        {
+        'Ldv': 'AA12345',
+        'ProcessedTime': '2019-07-03 17:37:00',
+        'Weight': 4.5,
+        'Width': 20.8,
+        'Lenght': 10.5,
+        'Height': 5.5,
+        'Accessory': 'GOL',
+        'AccessoryColorToShow': 'red',
+        'PostalServiceDescription': 'servizio Golden',
+        'PostalCode': '10042',
+        'DestinationId': '010',
+        'DestinationDescription': 'TF',
+        'DestinationBay': '001',
+        'SortingProcess': 'PARTENZE',
+        'SortedParcelImages': [
+            'https://www.fg-a.com/backgrounds/black-snakes-lg.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIvOrpbg4qC8bc4oPIPnBdv83j1skY1dCvKPrreCO_WqKO3XRAdg',
+            'http://paper-backgrounds.com/textureimages/2012/07/orange-grunge-background-hd-500x500.jpg'
+        ]
+        },
+        {
+        'Ldv': 'AA12346',
+        'ProcessedTime': '2019-07-03 17:37:00',
+        'Weight': 4.5,
+        'Width': 20.8,
+        'Lenght': 10.5,
+        'Height': 5.5,
+        'Accessory': 'SAB',
+        'AccessoryColorToShow': 'yellow',
+        'PostalServiceDescription': 'servizio al Sabato',
+        'PostalCode': '10042',
+        'DestinationId': '010',
+        'DestinationDescription': 'TF',
+        'DestinationBay': '001',
+        'SortingProcess': 'PARTENZE',
+        'SortedParcelImages': [
+            'https://www.fg-a.com/backgrounds/black-snakes-lg.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIvOrpbg4qC8bc4oPIPnBdv83j1skY1dCvKPrreCO_WqKO3XRAdg',
+            'http://paper-backgrounds.com/textureimages/2012/07/orange-grunge-background-hd-500x500.jpg'
+        ]
+        },
+        {
+        'Ldv': 'AA12347',
+        'ProcessedTime': '2019-07-03 17:37:00',
+        'Weight': 4.5,
+        'Width': 20.8,
+        'Lenght': 10.5,
+        'Height': 5.5,
+        'Accessory': 'APP',
+        'AccessoryColorToShow': 'purple',
+        'PostalServiceDescription': 'servizio su appuntamento',
+        'PostalCode': '10042',
+        'DestinationId': '010',
+        'DestinationDescription': 'TF',
+        'DestinationBay': '001',
+        'SortingProcess': 'PARTENZE',
+        'SortedParcelImages': [
+            'https://www.fg-a.com/backgrounds/black-snakes-lg.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIvOrpbg4qC8bc4oPIPnBdv83j1skY1dCvKPrreCO_WqKO3XRAdg',
+            'http://paper-backgrounds.com/textureimages/2012/07/orange-grunge-background-hd-500x500.jpg'
+        ]
+        }
+    ],
+    'CurrentPage': 2,
+    'TotalPages': 25
+}
--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import * as moment from 'moment';
+import { jsonData } from '../constant';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-dashboard',
@@ -13,15 +15,29 @@ export class DashboardComponent implements OnInit {
     myForm: FormGroup;
     isExpand = false;
     selectedBtn: any;
+    page = 1;
+    cercaData: any;
+    isCollapsed = false;
+    selectedIdx: any;
+    selectedImg: any;
+
+    constructor(private modalService: NgbModal) {}
 
     ngOnInit() {
+        this.cercaData = jsonData.LdvResults;
+        for (let item of this.cercaData) {
+            item.date = moment(new Date(item.ProcessedTime)).format('DD/MM/YYYY');
+            item.time = moment(new Date(item.ProcessedTime)).format('HH:mm:ss');
+            item.isCollapsed = false;
+        }
         let now = new Date();
         this.myForm = new FormGroup({
             ldv: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern(/^[\w\s]+$/), uppercaseValidator]),
             from_date: new FormControl(moment(now).subtract(7,'d').format('YYYY-MM-DD')),
             from_time: new FormControl(),
-            to_date: new FormControl(moment(now).format("YYYY-MM-DD")),
-            to_time: new FormControl()
+            to_date: new FormControl(moment(now).format('YYYY-MM-DD')),
+            to_time: new FormControl(),
+            itemsPerPage: new FormControl('5')            
         });
         let current_time = {
             hour: parseInt(moment(now).subtract(7,'d').format('HH')),
@@ -36,7 +52,7 @@ export class DashboardComponent implements OnInit {
         let now = new Date();
         let from_time = { hour: 0, minute: 0 }
         let to_time = { hour: 23, minute: 59 }
-        this.myForm.patchValue({from_date: moment(now).format("YYYY-MM-DD"), from_time: from_time, to_date: moment(now).format("YYYY-MM-DD"), to_time: to_time});
+        this.myForm.patchValue({from_date: moment(now).format('YYYY-MM-DD'), from_time: from_time, to_date: moment(now).format('YYYY-MM-DD'), to_time: to_time});
     }
 
     yesterday() {
@@ -44,7 +60,7 @@ export class DashboardComponent implements OnInit {
         let now = new Date();
         let from_time = { hour: 0, minute: 0 }
         let to_time = { hour: 23, minute: 59 }
-        this.myForm.patchValue({from_date: moment(now).subtract(1,'d').format("YYYY-MM-DD"), from_time: from_time, to_date: moment(now).format("YYYY-MM-DD"), to_time: to_time});
+        this.myForm.patchValue({from_date: moment(now).subtract(1,'d').format('YYYY-MM-DD'), from_time: from_time, to_date: moment(now).format('YYYY-MM-DD'), to_time: to_time});
     }
 
     last3Day() {
@@ -52,7 +68,7 @@ export class DashboardComponent implements OnInit {
         let now = new Date();
         let from_time = { hour: 0, minute: 0 }
         let to_time = { hour: 23, minute: 59 }
-        this.myForm.patchValue({from_date: moment(now).subtract(3,'d').format("YYYY-MM-DD"), from_time: from_time, to_date: moment(now).format("YYYY-MM-DD"), to_time: to_time});
+        this.myForm.patchValue({from_date: moment(now).subtract(3,'d').format('YYYY-MM-DD'), from_time: from_time, to_date: moment(now).format('YYYY-MM-DD'), to_time: to_time});
     }
 
     last7Day() {
@@ -60,7 +76,7 @@ export class DashboardComponent implements OnInit {
         let now = new Date();
         let from_time = { hour: 0, minute: 0 }
         let to_time = { hour: 23, minute: 59 }
-        this.myForm.patchValue({from_date: moment(now).subtract(7,'d').format("YYYY-MM-DD"), from_time: from_time, to_date: moment(now).format("YYYY-MM-DD"), to_time: to_time});
+        this.myForm.patchValue({from_date: moment(now).subtract(7,'d').format('YYYY-MM-DD'), from_time: from_time, to_date: moment(now).format('YYYY-MM-DD'), to_time: to_time});
     }
 
     lastMonth() {
@@ -68,26 +84,47 @@ export class DashboardComponent implements OnInit {
         let now = new Date();
         let from_time = { hour: 0, minute: 0 }
         let to_time = { hour: 23, minute: 59 }
-        this.myForm.patchValue({from_date: moment(now).subtract(1,'months').format("YYYY-MM-DD"), from_time: from_time, to_date: moment(now).format("YYYY-MM-DD"), to_time: to_time});
+        this.myForm.patchValue({from_date: moment(now).subtract(1,'months').format('YYYY-MM-DD'), from_time: from_time, to_date: moment(now).format('YYYY-MM-DD'), to_time: to_time});
     }
 
     reset(){
+        this.myForm.reset();
         this.selectedBtn = '';
+        this.isExpand = false;
         let now = new Date();
         let current_time = {
             hour: parseInt(moment(now).subtract(7,'d').format('HH')),
             minute: parseInt(moment(now).subtract(7,'d').format('mm'))
         }
-        this.myForm.patchValue({from_date: moment(now).subtract(7,'d').format('YYYY-MM-DD'), from_time: current_time, to_date: moment(now).format("YYYY-MM-DD"), to_time: current_time});
-    }
-    expand() {
-        if(this.myForm.valid){
-            this.isExpand = !this.isExpand
+        this.myForm.patchValue({ldv: '', from_date: moment(now).subtract(7,'d').format('YYYY-MM-DD'), from_time: current_time, to_date: moment(now).format('YYYY-MM-DD'), to_time: current_time, itemsPerPage: '5'});
+        for (let item of this.cercaData) {
+            item.isCollapsed = false;
         }
     }
 
+    expand() {
+        if (this.myForm.valid) {
+            this.isExpand = true;
+        }
+    }
+
+    open(content, image) {
+        this.selectedImg = image;
+        this.modalService.open(content, { windowClass: 'image-modal' });
+    }
+
     onSubmit(data) {
-        console.log(data);
+        if (this.myForm.valid) {
+            let jsnData = {
+                'LdvToFind': data.ldv,
+                'StartTimeSearch': data.from_date + ' ' + data.from_time.hour + ':' + data.from_time.minute + ':00',
+                'EndTimeSearch': data.to_date + ' ' + data.to_time.hour + ':' + data.to_time.minute + ':00',
+                'ApplicationType': 2,
+                'ItemsPerPage': data.itemsPerPage,
+                'Page': this.page           
+            };
+            console.log(jsnData);
+        }
     }
 }
 
